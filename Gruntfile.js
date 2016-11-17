353f62b9236db08c0de91cedbf0eef45b0f84b37
Eralp Karaduman <eralpkaraduman@gmail.com>
2016-11-17 20:03:24 +0300

    copy libs

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,24 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all : ['Gruntfile.js', 'public/**/*js', 'server/**/*js']
+      all : [
+        'Gruntfile.js',
+        'public/**/*js',
+        'server/**/*js',
+        '!public/lib/**'
+      ]
+    },
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            cwd: 'node_modules/phaser/build',
+            src: 'phaser.min.js',
+            dest: 'public/lib'
+          }
+        ]
+      }
     },
     connect: {
       server: {
@@ -23,8 +40,9 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['jshint', 'connect']);
+  grunt.registerTask('default', ['copy','jshint', 'connect']);
 };
--- app/server/boot.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(grunt) {
-
-  console.log("booting server");
-
-};
