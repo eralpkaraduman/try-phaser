cb22b60b15abbafebbe41d67fd00fc74f4b7f8e3
Eralp Karaduman <eralpkaraduman@gmail.com>
2016-11-18 04:41:51 +0300

    keyboard move

--- game/gameState.js
+++ game/gameState.js
@@ -1,9 +1,31 @@
 var Person = require('./person');
 
+var input = {};
+var player = null
+
+function create() {
+
+  var game = window.game;
+  player = new Person(game, this.world.centerX, this.world.centerY);
+
+  input.upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
+  input.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+  input.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+  input.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
+
+}
+
+function update() {
+
+  input.up = input.upKey.isDown;
+  input.down = input.downKey.isDown;
+  input.left = input.leftKey.isDown;
+  input.right = input.rightKey.isDown;
+
+  player.move(input);
+}
+
 module.exports = {
-
-  create: function() {
-
-    var player = new Person(window.game, this.world.centerX, this.world.centerY);
-  }
+  create: create,
+  update, update
 }
--- game/person.js
+++ game/person.js
@@ -1,4 +1,7 @@
 var Person = function(game, x, y) {
+
+  this.moveSpeed = 5;
+
   Phaser.Sprite.call(this, game, x, y, 'man-blue');
   this.anchor.setTo(0.5, 0.5);
   game.add.existing(this);
@@ -7,6 +10,33 @@ var Person = function(game, x, y) {
 Person.prototype = Object.create(Phaser.Sprite.prototype);
 Person.prototype.constructor = Person;
 Person.prototype.update = function() {
+
+}
+
+Person.prototype.move = function(input) {
+
+  var pos = {y: 0, x: 0};
+
+  if (input.up)
+  {
+      pos.y = -1;
+  }
+  else if (input.down)
+  {
+      pos.y = 1;
+  }
+
+  if (input.left)
+  {
+      pos.x = -1;
+  }
+  else if (input.right)
+  {
+      pos.x = 1;
+  }
+
+  this.x += (pos.x * this.moveSpeed);
+  this.y += (pos.y * this.moveSpeed);
 
 }
 
